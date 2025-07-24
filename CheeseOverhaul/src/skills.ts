125385ead3cf6cec951882d4dc26697b3cb71302
hiddenyak <hiddenyak@gmail.com>
2025-07-24 14:31:26 -0400

    refactor

--- CheeseOverhaul/src/skills.ts
+++ CheeseOverhaul/src/skills.ts
@@ -4,121 +4,101 @@ import { IDatabaseTables } from "@spt/models/spt/server/IDatabaseTables";
 import { ILogger } from "@spt/models/spt/utils/ILogger";
 
 export function handleSkills(tables: IDatabaseTables, logger: ILogger): void {
-	const globals = tables.globals;
+	const config = tables.globals.config;
 
 	//Disable skill fatigue
-	globals.config.SkillMinEffectiveness = 1;
-	globals.config.SkillFreshEffectiveness = 1;
+	config.SkillMinEffectiveness = 1;
+	config.SkillFreshEffectiveness = 1;
 
-	//Weapon Related
-	globals.config.SkillsSettings.Pistol.WeaponShotAction = 1.5;
-	globals.config.SkillsSettings.Revolver.WeaponShotAction = 1.5;
-	globals.config.SkillsSettings.DMR.WeaponShotAction = 1.5;
-	globals.config.SkillsSettings.Assault.WeaponShotAction = 1.5;
-	globals.config.SkillsSettings.Shotgun.WeaponShotAction = 1.5;
-	globals.config.SkillsSettings.Sniper.WeaponShotAction = 1.5;
+	//Weapon Skills
+	config.SkillsSettings.Pistol.WeaponShotAction = 1.5;
+	config.SkillsSettings.Revolver.WeaponShotAction = 1.5;
+	config.SkillsSettings.DMR.WeaponShotAction = 1.5;
+	config.SkillsSettings.Assault.WeaponShotAction = 1.5;
+	config.SkillsSettings.Shotgun.WeaponShotAction = 1.5;
+	config.SkillsSettings.Sniper.WeaponShotAction = 1.5;
 
-	//Charisma
-	globals.config.SkillsSettings.Charisma.BonusSettings.LevelBonusSettings.InsuranceDiscount = 0.01;
-	globals.config.SkillsSettings.Charisma.BonusSettings.LevelBonusSettings.RepeatableQuestChangeDiscount = 0.01;
-	globals.config.SkillsSettings.Charisma.BonusSettings.LevelBonusSettings.PaidExitDiscount = 0.01;
-	globals.config.SkillsSettings.Charisma.BonusSettings.LevelBonusSettings.HealthRestoreDiscount = 0.01;
-	globals.config.SkillsSettings.Charisma.BonusSettings.LevelBonusSettings.HealthRestoreTraderDiscount = 0.01;
-
-	//Mag Drills
-	globals.config.SkillsSettings.MagDrills.RaidUnloadedAmmoAction = 0.4;
-	globals.config.SkillsSettings.MagDrills.RaidLoadedAmmoAction = 0.6;
-
-	//Immunity
-	globals.config.SkillsSettings.Immunity.StimulatorNegativeBuff = 0.02;
-
-	//Endurance
-	globals.config.SkillsSettings.Endurance.SprintAction = 0.1;
-	globals.config.SkillsSettings.Endurance.MovementAction = 0.015;
-
-	//Strength
-	globals.config.SkillsSettings.Strength.ThrowAction = 4;
-	globals.config.SkillsSettings.Strength.SprintActionMax = 0.3;
-	globals.config.SkillsSettings.Strength.SprintActionMin = 0.1;
-	globals.config.SkillsSettings.Strength.MovementActionMax = 0.3;
-	globals.config.SkillsSettings.Strength.MovementActionMin = 0.1;
-
-	//Stress Resistance
-	globals.config.SkillsSettings.StressResistance.HealthNegativeEffect = 6;
-
-	//Vitality
-	globals.config.SkillsSettings.Vitality.DamageTakenAction = 0.2;
-
-	//Perception
-	globals.config.SkillsSettings.Perception.OnlineAction = 4;
-	globals.config.SkillsSettings.Perception.UniqueLoot = 0.5;
-
-	//Throwing
-	globals.config.SkillsSettings.Throwing.ThrowAction = 10;
-
-	//Recoil Control
-	globals.config.SkillsSettings.RecoilControl.RecoilBonusPerLevel = 0.005;
-	globals.config.SkillsSettings.RecoilControl.RecoilAction = 0.0006;
-
-	//Aim Drills
-	globals.config.SkillsSettings.AimDrills.WeaponShotAction = 2;
-
-	//Troubleshooting
-	globals.config.SkillsSettings.TroubleShooting.SkillPointsPerMalfFix = 200;
-
-	//Surgery
-	globals.config.SkillsSettings.Surgery.SurgeryAction = 100;
-
-	//Search
-	globals.config.SkillsSettings.Search.FindAction = 1.5;
-	globals.config.SkillsSettings.Search.SearchAction = 3;
-
-	//Covert Movement
-	globals.config.SkillsSettings.CovertMovement.MovementAction = 10;
+	//Character Skills
+	config.SkillsSettings.Charisma.BonusSettings.LevelBonusSettings.InsuranceDiscount = 0.01;
+	config.SkillsSettings.Charisma.BonusSettings.LevelBonusSettings.RepeatableQuestChangeDiscount = 0.01;
+	config.SkillsSettings.Charisma.BonusSettings.LevelBonusSettings.PaidExitDiscount = 0.01;
+	config.SkillsSettings.Charisma.BonusSettings.LevelBonusSettings.HealthRestoreDiscount = 0.01;
+	config.SkillsSettings.Charisma.BonusSettings.LevelBonusSettings.HealthRestoreTraderDiscount = 0.01;
+	config.SkillsSettings.MagDrills.RaidUnloadedAmmoAction = 0.4;
+	config.SkillsSettings.MagDrills.RaidLoadedAmmoAction = 0.6;
+	config.SkillsSettings.Immunity.StimulatorNegativeBuff = 0.02;
+	config.SkillsSettings.Endurance.SprintAction = 0.1;
+	config.SkillsSettings.Endurance.MovementAction = 0.015;
+	config.SkillsSettings.Strength.ThrowAction = 4;
+	config.SkillsSettings.Strength.SprintActionMax = 0.3;
+	config.SkillsSettings.Strength.SprintActionMin = 0.1;
+	config.SkillsSettings.Strength.MovementActionMax = 0.3;
+	config.SkillsSettings.Strength.MovementActionMin = 0.1;
+	config.SkillsSettings.StressResistance.HealthNegativeEffect = 6;
+	config.SkillsSettings.Vitality.DamageTakenAction = 0.2;
+	config.SkillsSettings.Perception.OnlineAction = 4;
+	config.SkillsSettings.Perception.UniqueLoot = 0.5;
+	config.SkillsSettings.Throwing.ThrowAction = 10;
+	config.SkillsSettings.RecoilControl.RecoilBonusPerLevel = 0.005;
+	config.SkillsSettings.RecoilControl.RecoilAction = 0.0006;
+	config.SkillsSettings.AimDrills.WeaponShotAction = 2;
+	config.SkillsSettings.TroubleShooting.SkillPointsPerMalfFix = 200;
+	config.SkillsSettings.Surgery.SurgeryAction = 100;
+	config.SkillsSettings.Search.FindAction = 1.5;
+	config.SkillsSettings.Search.SearchAction = 3;
+	config.SkillsSettings.CovertMovement.MovementAction = 10;
 
 	//Light Vests
-	globals.config.SkillsSettings.LightVests.MoveSpeedPenaltyReductionHVestsReducePerLevel = 0.02;
-	globals.config.SkillsSettings.LightVests.MeleeDamageLVestsReducePerLevel = 0.01;
-	globals.config.SkillsSettings.LightVests.BuffSettings.RareBuffChanceCoff = 0.4;
-	globals.config.SkillsSettings.LightVests.BuffSettings.MaxDurabilityLossToRemoveBuff = 0.5;
-	globals.config.SkillsSettings.LightVests.BuffSettings.CurrentDurabilityLossToRemoveBuff = 0.5;
-	globals.config.SkillsSettings.LightVests.MeleeDamageLVestsReducePerLevel = 0.01;
-	//BUGGED
-	globals.config.SkillsSettings.LightVests.WearChanceRepairLVestsReduceEliteLevel = 0.99;
-	globals.config.SkillsSettings.LightVests.WearAmountRepairLVestsReducePerLevel = 0.02;
+	config.SkillsSettings.LightVests.MoveSpeedPenaltyReductionHVestsReducePerLevel = 0.02;
+	config.SkillsSettings.LightVests.RicochetChanceHVestsCurrentDurabilityThreshold = 0.01;
+	config.SkillsSettings.LightVests.RicochetChanceHVestsEliteLevel = 0.3;
+	config.SkillsSettings.LightVests.RicochetChanceHVestsMaxDurabilityThreshold = 1;
+	config.SkillsSettings.LightVests.MeleeDamageLVestsReducePerLevel = 0.01;
+	config.SkillsSettings.LightVests.MoveSpeedPenaltyReductionLVestsReducePerLevel = 0.02;
+	config.SkillsSettings.LightVests.WearAmountRepairLVestsReducePerLevel = 0.02;
+	config.SkillsSettings.LightVests.WearChanceRepairLVestsReduceEliteLevel = 1;
+
+	//Light Vests Buffs
+	config.SkillsSettings.LightVests.BuffSettings.RareBuffChanceCoff = 0.4;
+	config.SkillsSettings.LightVests.BuffSettings.MaxDurabilityLossToRemoveBuff = 1;
+	config.SkillsSettings.LightVests.BuffSettings.CurrentDurabilityLossToRemoveBuff = 1;
 
 	//Heavy Vests
-	globals.config.SkillsSettings.HeavyVests.MoveSpeedPenaltyReductionHVestsReducePerLevel = 0.02;
-	globals.config.SkillsSettings.HeavyVests.RicochetChanceHVestsEliteLevel = 0.2;
-	globals.config.SkillsSettings.HeavyVests.BuffSettings.RareBuffChanceCoff = 0.4;
-	globals.config.SkillsSettings.HeavyVests.BuffSettings.MaxDurabilityLossToRemoveBuff = 0.5;
-	globals.config.SkillsSettings.HeavyVests.BuffSettings.CurrentDurabilityLossToRemoveBuff = 0.5;
-	//bug? property is named LVest
-	globals.config.SkillsSettings.HeavyVests.WearChanceRepairLVestsReduceEliteLevel = 1;
-	globals.config.SkillsSettings.HeavyVests.WearAmountRepairLVestsReducePerLevel = 0.02;
-	globals.config.SkillsSettings.HeavyVests.MeleeDamageLVestsReducePerLevel = 0.01;
+	config.SkillsSettings.HeavyVests.MoveSpeedPenaltyReductionHVestsReducePerLevel = 0.02;
+	config.SkillsSettings.HeavyVests.RicochetChanceHVestsCurrentDurabilityThreshold = 0.01;
+	config.SkillsSettings.HeavyVests.RicochetChanceHVestsEliteLevel = 0.3;
+	config.SkillsSettings.HeavyVests.RicochetChanceHVestsMaxDurabilityThreshold = 1;
+	config.SkillsSettings.HeavyVests.MeleeDamageLVestsReducePerLevel = 0.01;
+	config.SkillsSettings.HeavyVests.MoveSpeedPenaltyReductionLVestsReducePerLevel = 0.02;
+	config.SkillsSettings.HeavyVests.WearAmountRepairLVestsReducePerLevel = 0.02;
+	config.SkillsSettings.HeavyVests.WearChanceRepairLVestsReduceEliteLevel = 1;
+
+	// Heavy Vests Buffs
+	config.SkillsSettings.HeavyVests.BuffSettings.RareBuffChanceCoff = 0.4;
+	config.SkillsSettings.HeavyVests.BuffSettings.MaxDurabilityLossToRemoveBuff = 1;
+	config.SkillsSettings.HeavyVests.BuffSettings.CurrentDurabilityLossToRemoveBuff = 1;
 
 	//Weapon Maintenance
-	globals.config.SkillsSettings.WeaponTreatment.DurLossReducePerLevel = 0.01;
-	globals.config.SkillsSettings.WeaponTreatment.SkillPointsPerRepair = 20;
-	globals.config.SkillsSettings.WeaponTreatment.WearAmountRepairGunsReducePerLevel = 0.02;
-	globals.config.SkillsSettings.WeaponTreatment.WearChanceRepairGunsReduceEliteLevel = 1;
-	globals.config.SkillsSettings.WeaponTreatment.BuffSettings.CurrentDurabilityLossToRemoveBuff = 0.5;
-	globals.config.SkillsSettings.WeaponTreatment.BuffSettings.MaxDurabilityLossToRemoveBuff = 0.5;
+	config.SkillsSettings.WeaponTreatment.DurLossReducePerLevel = 0.01;
+	config.SkillsSettings.WeaponTreatment.SkillPointsPerRepair = 50;
+	config.SkillsSettings.WeaponTreatment.WearAmountRepairGunsReducePerLevel = 0.02;
+	config.SkillsSettings.WeaponTreatment.WearChanceRepairGunsReduceEliteLevel = 1;
+	config.SkillsSettings.WeaponTreatment.BuffSettings.CurrentDurabilityLossToRemoveBuff = 0.5;
+	config.SkillsSettings.WeaponTreatment.BuffSettings.MaxDurabilityLossToRemoveBuff = 0.5;
 
 	//Crafting
-	globals.config.SkillsSettings.Crafting.PointsPerCraftingCycle = 200;
-	globals.config.SkillsSettings.Crafting.PointsPerUniqueCraftCycle = 1000;
-	globals.config.SkillsSettings.Crafting.ProductionTimeReductionPerLevel = 1.6;
-	globals.config.SkillsSettings.Crafting.CraftTimeReductionPerLevel = 1.6;
-	globals.config.SkillsSettings.Crafting.EliteExtraProductions = 2;
+	config.SkillsSettings.Crafting.PointsPerCraftingCycle = 1000;
+	config.SkillsSettings.Crafting.PointsPerUniqueCraftCycle = 1000;
+	config.SkillsSettings.Crafting.ProductionTimeReductionPerLevel = 1.6;
+	config.SkillsSettings.Crafting.CraftTimeReductionPerLevel = 1.6;
+	config.SkillsSettings.Crafting.EliteExtraProductions = 2;
 
-	//Hideout Managment
-	globals.config.SkillsSettings.HideoutManagement.SkillPointsPerAreaUpgrade = 100;
-	globals.config.SkillsSettings.HideoutManagement.SkillPointsPerCraft = 5;
-	globals.config.SkillsSettings.HideoutManagement.EliteSlots.BitcoinFarm.Container = 20;
-	globals.config.SkillsSettings.HideoutManagement.EliteSlots.Generator.Slots = 4;
-	globals.config.SkillsSettings.HideoutManagement.EliteSlots.WaterCollector.Slots = 4;
+	//Hideout Management
+	config.SkillsSettings.HideoutManagement.SkillPointsPerAreaUpgrade = 100;
+	config.SkillsSettings.HideoutManagement.SkillPointsPerCraft = 5;
+	config.SkillsSettings.HideoutManagement.EliteSlots.BitcoinFarm.Container = 20;
+	config.SkillsSettings.HideoutManagement.EliteSlots.Generator.Slots = 4;
+	config.SkillsSettings.HideoutManagement.EliteSlots.WaterCollector.Slots = 4;
 
 	logger.logWithColor(
 		"Handled Skills",
